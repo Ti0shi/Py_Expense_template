322b1741ff5a0eccb44794e1bfd6d70a17d8a44e
Ti0shi <qf200022@gmail.com>
2023-09-20 20:22:21 +0200

    Update csv writer

--- expense.py
+++ expense.py
@@ -1,3 +1,5 @@
+import csv
+
 from PyInquirer import prompt
 
 expense_questions = [
@@ -24,10 +26,10 @@ expense_questions = [
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    with open('expense_report.csv', 'a') as file:
-        writer = csv.writer(file, dialect='unix')
-        for i in infos:
-            writer.writerow([infos[i]])
+    with open('expense_report.csv', 'a', newline='') as file:
+        fieldsname = ["amount", "label", "spender"]
+        writer = csv.DictWriter(file, fieldsname)
+        writer.writerow(infos)
     print("Expense Added !")
     return True
 
